43726e8522c7bd9be02dcb541a14a5816bb064b7
Dominicentek <dominicentekgaming@gmail.com>
2024-08-04 14:50:53 +0200

    black borders

--- src/main.c
+++ src/main.c
@@ -41,26 +41,25 @@ void drawlist_renderer(void* texture, float dstx, float dsty, float dstw, float 
     SDL_RenderCopyF(renderer, texture, &src, &dst);
 }
 
-void adjust_display(int width, int height) {
+void adjust_display(int width, int height, int* new_w, int* new_h) {
     float aspect_ratio = width / (float)height;
     int w, h;
     SDL_GetWindowSize(window, &w, &h);
     if (w == 0) w = 1; // prevent division by 0
     if (h == 0) h = 1;
     float scr_aspect_ratio = w / (float)h;
-    int new_w, new_h;
     if (aspect_ratio > scr_aspect_ratio) {
-        new_w = w;
-        new_h = w / aspect_ratio;
-        scale = w / (float)width;
+        *new_w = w;
+        *new_h = w / aspect_ratio;
+         scale = w / (float)width;
     }
     else {
-        new_h = h;
-        new_w = h * aspect_ratio;
-        scale = h / (float)height;
+        *new_h = h;
+        *new_w = h * aspect_ratio;
+         scale = h / (float)height;
     }
-    translate_x = (w - new_w) / 2.f;
-    translate_y = (h - new_h) / 2.f;
+    translate_x = (w - *new_w) / 2.f;
+    translate_y = (h - *new_h) / 2.f;
 }
 
 int main() {
@@ -68,6 +67,7 @@ int main() {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     window = SDL_CreateWindow("Super Mario Bros. Reimagined", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+    SDL_SetWindowMinimumSize(window, WIDTH, HEIGHT);
     audio_init();
     load_assets(renderer);
     init_data();
@@ -77,7 +77,10 @@ int main() {
     while (true) {
         if (handle_sdl_events()) break;
         Uint64 frame = frame_begin();
-        adjust_display(WIDTH, HEIGHT);
+        int frame_w, frame_h;
+        int width, height;
+        SDL_GetWindowSize(window, &width, &height);
+        adjust_display(WIDTH, HEIGHT, &frame_w, &frame_h);
         SDL_SetRenderDrawColor(renderer, 127, 127, 127, 255);
         SDL_RenderClear(renderer);
         if (current_level != NULL) {
@@ -86,6 +89,15 @@ int main() {
             LE_Render(drawlist, drawlist_renderer);
             LE_ClearDrawList(drawlist);
         }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_Rect rect = { 0, 0, translate_x, height };
+        SDL_RenderFillRect(renderer, &rect);
+        rect.x = 0; rect.y = 0; rect.w = width, rect.h = translate_y;
+        SDL_RenderFillRect(renderer, &rect);
+        rect.x = width - translate_x; rect.y = 0; rect.w = translate_x, rect.h = height;
+        SDL_RenderFillRect(renderer, &rect);
+        rect.x = 0; rect.y = height - translate_y; rect.w = width, rect.h = translate_y;
+        SDL_RenderFillRect(renderer, &rect);
         SDL_RenderPresent(renderer);
         frame_end(frame, FPS);
     }
