a285681dd60f0800acfc45dd389e8d7748c1b5b9
Dominicentek <dominicentekgaming@gmail.com>
2024-10-31 12:12:44 +0100

    big endian hardware support

---
+++ include/endianness.h
@@ -0,0 +1,24 @@
+#ifndef ENDIANNESS_H
+#define ENDIANNESS_H
+
+#define FLIP_ENDIAN(x) ({                \
+    union {                               \
+        char arr[sizeof(x)];               \
+        typeof(x) val;                      \
+    } in, out;                               \
+    in.val = (x);                             \
+    for (int i = 0; i < sizeof(x); i++) {      \
+        out.arr[sizeof(x) - 1 - i] = in.arr[i]; \
+    }                                            \
+    out.val;                                      \
+})
+
+#ifdef __LITTLE_ENDIAN__
+#define BE(x) FLIP_ENDIAN(x)
+#define LE(x) (x)
+#else
+#define BE(x) (x)
+#define LE(x) FLIP_ENDIAN(x)
+#endif
+
+#endif
