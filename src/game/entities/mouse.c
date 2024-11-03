97235d7b6d982aa91af1b7208665b035ec1efd2e
Dominicentek <dominicentekgaming@gmail.com>
2024-11-03 14:27:00 +0100

    mouse squish animation

--- src/game/entities/mouse.c
+++ src/game/entities/mouse.c
@@ -16,13 +16,16 @@ entity_texture(squashed_mouse) {
     *srcH = 16;
     *w = 16;
     *h = 16;
+    entity_apply_squish(entity, w, h);
     return GET_ASSET(struct Texture, "images/entities/mouse.png");
 }
 
 entity_update(squashed_mouse) {
+    if (entity_init(entity)) LE_EntitySetProperty(entity, (LE_EntityProperty){ .asFloat = .5f }, "squish");
     LE_EntityProperty timer = (LE_EntityProperty){ .asInt = 60 };
     LE_EntityGetProperty(entity, &timer, "timer");
     timer.asInt--;
     if (timer.asInt == 0) LE_DeleteEntity(entity);
     LE_EntitySetProperty(entity, timer, "timer");
+    entity_update_squish(entity, 5);
 }
